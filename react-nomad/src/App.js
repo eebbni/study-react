7802a9d8c54e1f545d6e13d6172ac66d16012db3
chaeyeon <engeliss@naver.com>
2021-05-03 18:00:57 +0900

    Food function add

--- react-nomad/src/App.js
+++ react-nomad/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
+function Food({fav}){
+  return (
+    <h3>I love {fav}</h3>
+  );
+}
+
 function App() {
   return (
     <div>
       <h1>Hello!!</h1>
+      <Food fav="Kimchi" />
+      <Food fav="ahi" />
+      <Food fav="Kfhi" />
     </div>
   );
 }
