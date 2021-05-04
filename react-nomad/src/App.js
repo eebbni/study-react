165e7196eb202dbaab8a520d94605a576c4dd807
chaeyeon <engeliss@naver.com>
2021-05-04 17:59:30 +0900

    node module install and App.js clear

--- react-nomad/src/App.js
+++ react-nomad/src/App.js
@@ -1,20 +1,11 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
-function Food({fav}){
-  return (
-    <h3>I love {fav}</h3>
-  );
+class App extends React.Component{
+  render(){
+    return <h1>I am a class component</h1>
+  }
 }
 
-function App() {
-  return (
-    <div>
-      <h1>Hello!!</h1>
-      <Food fav="Kimchi" />
-      <Food fav="ahi" />
-      <Food fav="Kfhi" />
-    </div>
-  );
-}
 
 export default App;
