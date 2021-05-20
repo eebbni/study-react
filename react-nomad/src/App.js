481b28329771f93fef8d412e6ab94e759dd1613c
chaeyeon <engeliss@naver.com>
2021-05-20 17:59:20 +0900

    install axios use await/async

--- react-nomad/src/App.js
+++ react-nomad/src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import PropTypes from "prop-types";
+import axios from "axios";
 
 class App extends React.Component{
   state = {
     isLoading : true,
     movies : []
   };
-  componentDidMount(){
-    setTimeout(()=>{
-      this.setState({isLoading : false});
-    },6000);
+  getMovies = async() =>{
+    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+  }
+  async componentDidMount(){
+    this.getMovies();
   }
   render(){
    const {isLoading} = this.state;
